e8b3d33bd7eb11c840a3134adb3b5ea310964c53
Sam Vanderbruggen <contact@saze.me>
2022-11-25 00:40:50 +0100

    fix indent

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ app.post('/gameserverconnector-webapp-fiesta-us/sso', (req, res) => {
   <?xml version="1.0" encoding="UTF-8"?>
   <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:ns1="http://infrastructure.games.gamigo.com/v_1_3">
     <SOAP-ENV:Body>
-  	  <ns1:getItemShopUrlResponse>
-  	    <ns1:url>${url}</ns1:url>
-  	  </ns1:getItemShopUrlResponse>
-  	</SOAP-ENV:Body>
+      <ns1:getItemShopUrlResponse>
+        <ns1:url>${url}</ns1:url>
+      </ns1:getItemShopUrlResponse>
+    </SOAP-ENV:Body>
   </SOAP-ENV:Envelope>
   `);
 });
