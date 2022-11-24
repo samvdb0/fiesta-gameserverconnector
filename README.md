a91bf380ddf7e8c93c2b3f32d7dda2684c64cd6f
Sam Vanderbruggen <contact@saze.me>
2022-11-25 00:30:00 +0100

    push source

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # fiesta-gameserverconnector
-A barebones (WIP) reimplementation of the SOAP service that Fiesta's server services interface with for item mall-related operations. Currently, only the request for the item mall URL is handled without the account ID.
+A barebones (WIP) reimplementation of the SOAP service that Fiesta's server services interface with for item mall-related operations.
+Currently, only the request for the item mall URL is handled (without the account ID).
 
 This fixes the "Go to Fiesta Store" item on the `X`-menu without a hook, by reimplementing the internal Gamigo service.
 
-The URL can be set in app.js on line 11:
-`const url = "https://your_url.here";`
+## Usage
+1. Clone this repository to a folder
+2. Set the URL to your item mall in app.js on line 11: `const url = "https://your_url.here";`
+3. Add a `C:\Windows\System32\drivers\etc\hosts` entry for the original domain: `127.0.0.1 bip.srv.gsc.gas.gamigo.com`
+4. Run `npm i` inside the cloned folder to fetch all dependencies
+5. Run `node .` inside the cloned folder to start the server
---
+++ app.js
@@ -0,0 +1,41 @@
+// fiesta-gameserverconnector - Sam "saze" V. 2022
+
+const xml = require('xml');
+const parser = require('body-parser');
+require('body-parser-xml')(parser);
+
+const app = require('express')();
+app.use(parser.xml());
+
+// edit this property to your preferred item mall URL.
+const url = "https://your_url.here";
+
+app.post('/gameserverconnector-webapp-fiesta-us/sso', (req, res) => {
+  const body = req.body['SOAP-ENV:Envelope']['SOAP-ENV:Body'][0];
+  const requestType = Object.keys(body)[0];
+  const arguments = body[requestType][0];
+  
+  console.log(`Request: "${requestType}"`);
+  console.log("Arguments: %j", arguments); // args
+  
+  // it's also possible to use the accountId argument to send back an URL with 
+  // a temporary token for the user to log in directly into the mall on page load.
+  // const accountId = arguments["ns1:accountId"][0];
+  // console.log(accountId);
+  
+  res.set('Content-Type', 'text/xml');  
+  res.send(`
+  <?xml version="1.0" encoding="UTF-8"?>
+  <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:ns1="http://infrastructure.games.gamigo.com/v_1_3">
+    <SOAP-ENV:Body>
+  	  <ns1:getItemShopUrlResponse>
+  	    <ns1:url>${url}</ns1:url>
+  	  </ns1:getItemShopUrlResponse>
+  	</SOAP-ENV:Body>
+  </SOAP-ENV:Envelope>
+  `);
+});
+
+app.listen(8080, () => {
+  console.log(`Listening on 8080`);
+});
